2a78f390f1da37d6dea532845716cbac8e5a4bb0
the3musketeersgh <the3musketeersgh@gmail.com>
2024-08-31 20:01:51 +0000

    added title

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -15,7 +15,7 @@ function Player({initialName, symbol, isActive, onChangeName}) {
     setPlayerName(event.target.value);
   }
   
-let editablePlayerName = <span className='player-name'>{initialName}</span>;
+let editablePlayerName = <span className='player-name'>{playerName}</span>;
 
 if(isEditing){
   editablePlayerName = <input type="text" required value={playerName} onChange={handleChange}/>;
