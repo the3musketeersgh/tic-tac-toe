6e0526a6b062daecf51ed73152459cc1402e56f0
the3musketeersgh <the3musketeersgh@gmail.com>
2024-08-31 11:51:00 +0000

    added Player component

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Player from './components/Player'
 
 function App() {
  
   return (
   <main>
-<div id="game-container">
+<div id="game-container"> 
 
 <ol id="players">
-  <li><span className='player-name'>Player 1</span>
-  <span className='player-symbol'>X</span></li>
-  <li><span className='player-name'>Player 2</span>
-  <span className='player-symbol'>O</span></li>
+<Player name="Player 1" symbol="X"/>
+<Player name="Player 2" symbol="O"/>
 </ol>
 
 
