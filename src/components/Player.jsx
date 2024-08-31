ef9faff34e643eb51ca7ace455317d02468310d5
the3musketeersgh <the3musketeersgh@gmail.com>
2024-08-31 12:17:34 +0000

    show input component onclick

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -3,16 +3,23 @@ import React, { useState } from 'react'
 function Player({name, symbol}) {
   const [isEditing, setIsEditting] = useState(false);
   
-  function handleClick(){
+  function handleClick1(){
     setIsEditting(true);
   }
   
+let playerName = <span className='player-name'>{name}</span>;
+
+if(isEditing){
+  playerName = <input type="text" required/>;
+}
+
   return (
     <li><span className='player'>
-      {isEditing ? <span className='player-name'>{name}</span> : <input type="text" required/>}
+      {playerName}
+      {/* {isEditing ? <span className='player-name'>{name}</span> : <input type="text" required/>} */}
       <span className='player-symbol'>{symbol}</span>
   </span>
-  <button onClick={handleClick}>Edit</button>
+  <button onClick={handleClick1}>Edit</button>
   </li>
   )
 }
