a0c345142886c6733dff1b21adca66889d942397
the3musketeersgh <the3musketeersgh@gmail.com>
2024-08-31 11:31:19 +0000

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+ 
+  return (
+  <main>
+<div id="game-container">
+
+<ol id="players">
+  <li><span className='player-name'>Player 1</span>
+  <span className='player-symbol'>X</span></li>
+  <li><span className='player-name'>Player 2</span>
+  <span className='player-symbol'>O</span></li>
+</ol>
+
+
+GAME BOARD
+</div>
+
+  </main>
+  )
+}
+
+export default App
